f761999e07bb439132a4084b43a4d4765193bea2
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-05-02 13:24:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let count = {};
+function firstUniqueChar(str) {
+  for (let i=0, i<str.length, i++){
+    let char = str(i);
+    if 
+  }
+}
+
+str = "shockness";
+console.log(firstUniqueChar(str));
