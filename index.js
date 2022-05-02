5a1c2c38599df6d5d9fabd72748a4be5a8c090fe
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-05-02 14:11:07 +0000

    first unique character problem solved

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 let count = {};
 function firstUniqueChar(str) {
-  for (let i=0, i<str.length, i++){
-    let char = str(i);
-    if 
+  for(let i=0; i<str.length; i++){
+    let char = str[i];
+    if(!(char in count)) {
+      count[char] = 1;
+    } else {
+      count[char] = count[char] + 1;
+    }
   }
+
+  for(let i=0; i<str.length; i++) {
+    let char = str[i];
+    if (count[char] === 1) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
-str = "shockness";
-console.log(firstUniqueChar(str));
+str1 = "shockness";
+str2 = "leetcode";
+console.log(firstUniqueChar(str1));
+console.log(firstUniqueChar(str2));
