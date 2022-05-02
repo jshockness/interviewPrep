d99366d77522c052c6e0638844739d2f49f0b7af
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-05-02 14:50:49 +0000

    Valid Parens solution

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let count = {};
 function firstUniqueChar(str) {
+  let count = {};
   for(let i=0; i<str.length; i++){
     let char = str[i];
     if(!(char in count)) {
@@ -23,3 +23,30 @@ str1 = "shockness";
 str2 = "leetcode";
 console.log(firstUniqueChar(str1));
 console.log(firstUniqueChar(str2));
+
+
+function validParens(str) {
+  let hashTable = {"{":"}", "[":"]", "(":")"};
+  let stack = [];
+
+  for(let i=0; i<str.length; i++) {
+    let char = str[i]
+    if(char in hashTable) {
+      stack.push(hashTable[char]);
+    } else if (stack.length > 0 && stack[stack.length -1] === char) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  if(stack.length === 0) {
+    return true;
+  } else{
+    return false;
+  }
+}
+
+str3 = "([]){}[]{()}";
+str4 = "{[()]}{()}";
+console.log(validParens(str3));
+console.log(validParens(str4));
