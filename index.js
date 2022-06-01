78dbfeede749e6596c715449b449ade388aa6d95
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-06-01 19:30:03 +0000

    idk

--- index.js
+++ index.js
@@ -50,3 +50,30 @@ str3 = "([]){}[]{()}";
 str4 = "{[()]}{()}";
 console.log(validParens(str3));
 console.log(validParens(str4));
+
+// Recursive
+function uniquePaths(matrix) {
+
+  function traverse(x, y) {
+    if (x < 0 || y < 0 || x >= matrix[0].length || y >= matrix.length) {
+      return 0;
+    } else if (matrix[y][x] === 1) {
+      return 0;
+    } else if (x === matrix[0].length - 1 && y === matrix.length - 1) {
+      return 1;
+    }
+
+    matrix[y][x] = 1;
+    let sum = 0;
+    sum += traverse(x+1, y);
+    sum += traverse(x-1, y);
+    sum += traverse(x, y+1);
+    sum += traverse(x, y-1);
+    matrix[y][x] = 0;
+    return sum;
+  }
+  return traverse(0, 0);
+}
+matrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+
+console.log(uniquePaths(matrix));
